363a8a8fe44e3c498718f44dd28eea0bc945d1a7
Szymon Miękus <miekusoft@gmail.com>
2023-02-21 18:56:56 +0100

    DeletePackingList Handler

---
+++ Warehouse.Application/Commands/Handlers/DeletePackingListHandler.cs
@@ -0,0 +1,25 @@
+﻿using Warehouse.Application.Exceptions;
+using Warehouse.Domain.Repositories;
+using Warehouse.Shared.Abstractions.Commands;
+
+namespace Warehouse.Application.Commands.Handlers
+{
+    internal sealed class DeletePackingListHandler : ICommandHandler<DeletePackingList>
+    {
+        private readonly IPackingListRepository _repository;
+
+        public DeletePackingListHandler(IPackingListRepository repository) => _repository = repository;
+
+        public async Task HandleAsync(DeletePackingList command)
+        {
+            var packingList = await _repository.GetAsync(command.Id);
+
+            if (packingList is null)
+            {
+                throw new PackingListNotFoundExcepiotn(command.Id);
+            }
+
+            await _repository.DeleteAsync(packingList);
+        }
+    }
+}
---
+++ Warehouse.Application/Exceptions/PackingListNotFoundExcepiotn.cs
@@ -0,0 +1,16 @@
+﻿using Warehouse.Shared.Abstractions.Exceptions;
+
+namespace Warehouse.Application.Exceptions
+{
+    public class PackingListNotFoundExcepiotn : WarehouseExcepion
+    {
+        public Guid Name { get; }
+
+        public PackingListNotFoundExcepiotn(Guid name)
+            : base($"Packing list with name '{name}' not found.")
+        {
+            Name = name;
+        }
+
+    }
+}
