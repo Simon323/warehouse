7cb2d84bcc2e87645a4663eba41005caf55974ec
Szymon Miękus <miekusoft@gmail.com>
2023-07-06 16:44:50 +0200

    Prepare UnitTest proj & write first test

---
+++ Warehouse.UnitTests/Domain/PackingListTests.cs
@@ -0,0 +1,44 @@
+﻿using Shouldly;
+using Warehouse.Domain.Entities;
+using Warehouse.Domain.Exceptions;
+using Warehouse.Domain.Factories;
+using Warehouse.Domain.Policies;
+using Warehouse.Domain.ValueObjects;
+
+namespace Warehouse.UnitTests.Domain
+{
+    public class PackingListTests
+    {
+        [Fact]
+        public void AddItem_Throws_PackingItemAlreadyExistsException_When_There_Is_Already_Item_With_The_Same_Name()
+        {
+            // ARRANGE
+            var packingList = GetPackingList();
+            packingList.AddItem(new PackingItem("Item 1", 1));
+
+            // ACT
+            var exception = Record.Exception(() => packingList.AddItem(new PackingItem("Item 1", 1)));
+
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<PackingItemAlreadyExistsException>();
+        }
+
+        #region ARRANGE
+
+        private PackingList GetPackingList()
+        {
+            var packingList = _factory.Create(Guid.NewGuid(), "MyList", Localization.Create("Warsaw, Poland"));
+            packingList.ClearEvents();
+            return packingList;
+        }
+
+        private readonly IPackingListFactory _factory;
+
+        public PackingListTests()
+        {
+            _factory = new PackingListFactory(Enumerable.Empty<IPackingItemsPolicy>());
+        }
+
+        #endregion
+    }
+}
