3c623d6790e1b71a1da59ddae2d82869cb0a19ff
Szymon Miękus <miekusoft@gmail.com>
2023-07-06 15:01:36 +0200

    Add a few new api endpoints

--- Warehouse.Api/Controllers/PackingListsController.cs
+++ Warehouse.Api/Controllers/PackingListsController.cs
@@ -35,10 +35,38 @@ namespace Warehouse.Api.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(CreatePackingListWithItems command)
+        public async Task<IActionResult> Post([FromBody] CreatePackingListWithItems command)
         {
             await _commandDispatcher.DispatchAsync(command);
             return CreatedAtAction(nameof(Get), new { id = command.Id }, null);
+        }
+
+        [HttpPut("{packingListId}/items")]
+        public async Task<IActionResult> Put([FromBody] AddPackingItem command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok();
+        }
+
+        [HttpPut("{packingListId:guid}/items/{name}/pack")]
+        public async Task<IActionResult> Put([FromBody] PackItem command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok();
+        }
+
+        [HttpDelete("{packingListId:guid}/items/{name}")]
+        public async Task<IActionResult> Delete([FromBody] RemovePackingItem command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete([FromBody] RemovePackingList command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok();
         }
     }
 }
--- Warehouse.Application/Commands/RemovePackingList.cs
+++ Warehouse.Application/Commands/RemovePackingList.cs
@@ -2,5 +2,5 @@
 
 namespace Warehouse.Application.Commands
 {
-    public record DeletePackingList(Guid Id) : ICommand;
+    public record RemovePackingList(Guid Id) : ICommand;
 }
