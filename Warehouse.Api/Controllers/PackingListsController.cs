6b0bdde2e903ab47cfca49ab1ae62b0d5aa6b135
Szymon Miękus <miekusoft@gmail.com>
2023-07-06 14:19:15 +0200

    Create API controllers

---
+++ Warehouse.Api/Controllers/PackingListsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Warehouse.Application.Commands;
+using Warehouse.Application.DTO;
+using Warehouse.Application.Queries;
+using Warehouse.Shared.Abstractions.Commands;
+using Warehouse.Shared.Abstractions.Queries;
+
+namespace Warehouse.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PackingListsController : BaseController
+    {
+        private readonly IQueryDispatcher _queryDispatcher;
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public PackingListsController(IQueryDispatcher queryDispatcher, ICommandDispatcher commandDispatcher)
+        {
+            _queryDispatcher = queryDispatcher;
+            _commandDispatcher = commandDispatcher;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<PackingListDto>> Get([FromRoute] GetPackingList query)
+        {
+            var result = await _queryDispatcher.QueryAsync(query);
+            return OkOrNotFound(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PackingListDto>>> Get([FromRoute] SearchPackingLists query)
+        {
+            var result = await _queryDispatcher.QueryAsync(query);
+            return OkOrNotFound(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreatePackingListWithItems command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return CreatedAtAction(nameof(Get), new { id = command.Id }, null);
+        }
+    }
+}
