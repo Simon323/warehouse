d6a6d7eb138b257e10553909fdbf3c01185b886a
Szymon Miękus <miekusoft@gmail.com>
2023-07-06 17:24:32 +0200

    Tests for Application layer

---
+++ Warehouse.UnitTests/Application/CreatePackingListWithItemsHandlerTests.cs
@@ -0,0 +1,84 @@
+﻿using NSubstitute;
+using Shouldly;
+using Warehouse.Application.Commands;
+using Warehouse.Application.Commands.Handlers;
+using Warehouse.Application.DTO.External;
+using Warehouse.Application.Exceptions;
+using Warehouse.Application.Services;
+using Warehouse.Domain.Consts;
+using Warehouse.Domain.Entities;
+using Warehouse.Domain.Factories;
+using Warehouse.Domain.Repositories;
+using Warehouse.Domain.ValueObjects;
+using Warehouse.Shared.Abstractions.Commands;
+
+namespace Warehouse.UnitTests.Application
+{
+    public class CreatePackingListWithItemsHandlerTests
+    {
+        Task Act(CreatePackingListWithItems command)
+            => _commandHandler.HandleAsync(command);
+
+        [Fact]
+        public async Task HandleAsync_Throws_PackingListAlreadyExistsException_When_List_With_Same_Name_Already_Exists()
+        {
+            var command = new CreatePackingListWithItems(Guid.NewGuid(), "MyList", 10, Gender.Female, default);
+            _readService.ExistsByNameAsync(command.Name).Returns(true);
+
+            var exception = await Record.ExceptionAsync(() => Act(command));
+
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<PackingListAlreadyExistsException>();
+        }
+
+        [Fact]
+        public async Task HandleAsync_Throws_MissingLocalizationWeatherException_When_Weather_Is_Not_Returned_From_Service()
+        {
+            var command = new CreatePackingListWithItems(Guid.NewGuid(), "MyList", 10, Gender.Female, new LocalizationWriteModel("Warsaw", "Poland"));
+
+            _readService.ExistsByNameAsync(command.Name).Returns(false);
+            _weatherService.GetWeatherAsync(Arg.Any<Localization>()).Returns(default(WeatherDto));
+
+            var exception = await Record.ExceptionAsync(() => Act(command));
+
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<MissingLocalizationWeatherException>();
+        }
+
+        [Fact]
+        public async Task HandleAsync_Calls_Repository_On_Success()
+        {
+            var command = new CreatePackingListWithItems(Guid.NewGuid(), "MyList", 10, Gender.Female, new LocalizationWriteModel("Warsaw", "Poland"));
+
+            _readService.ExistsByNameAsync(command.Name).Returns(false);
+            _weatherService.GetWeatherAsync(Arg.Any<Localization>()).Returns(new WeatherDto(12));
+            _factory.CreateWithDefaultItems(command.Id, command.Name, command.Days, command.Gender, Arg.Any<Temperature>(), Arg.Any<Localization>()).Returns(default(PackingList));
+
+            var exception = await Record.ExceptionAsync(() => Act(command));
+
+            exception.ShouldBeNull();
+            await _repository.Received(1).AddAsync(Arg.Any<PackingList>());
+
+        }
+
+        #region ARRANGE
+
+        private readonly ICommandHandler<CreatePackingListWithItems> _commandHandler;
+        private readonly IPackingListRepository _repository;
+        private readonly IWeatherService _weatherService;
+        private readonly IPackingListReadService _readService;
+        private readonly IPackingListFactory _factory;
+
+        public CreatePackingListWithItemsHandlerTests()
+        {
+            _repository = Substitute.For<IPackingListRepository>();
+            _weatherService = Substitute.For<IWeatherService>();
+            _readService = Substitute.For<IPackingListReadService>();
+            _factory = Substitute.For<IPackingListFactory>();
+
+            _commandHandler = new CreatePackingListWithItemsHandler(_repository, _factory, _readService, _weatherService);
+        }
+
+        #endregion
+    }
+}
